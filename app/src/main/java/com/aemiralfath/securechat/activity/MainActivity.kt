25a9e61aea14c6cde6b824bf3869469d4dfb943f
criptdestroyer <ahmademir1201@gmail.com>
2021-02-12 23:47:07 +0700

    add menu;next;finishing add room layout

--- app/src/main/java/com/aemiralfath/securechat/activity/MainActivity.kt
+++ app/src/main/java/com/aemiralfath/securechat/activity/MainActivity.kt
@@ -2,11 +2,45 @@ package com.aemiralfath.securechat.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.aemiralfath.securechat.R
+import com.aemiralfath.securechat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var title = "Chat Room"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvRoom.setHasFixedSize(true)
+        supportActionBar?.title = title
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu_form, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.friend_list -> {
+
+            }
+            R.id.search_friend -> {
+
+            }
+            R.id.settings -> {
+
+            }
+        }
     }
 }
